4ae3a4c82f4787189d449b5662550e387cdee591
lyskz <lyski.zach@icloud.com>
2023-02-15 04:19:03 -0800

    started working on js

---
+++ script.js
@@ -0,0 +1,8 @@
+const start = document.getElementById("start-btn");
+const quizQuestionsBox = document.getElementById("question-container");
+const quizQuestion = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const leaderBoard = document.getElementById("high-score-board")
+
+var highScoreSubmission = document.getElementById("high-score-submission");
+var scoreBoard
