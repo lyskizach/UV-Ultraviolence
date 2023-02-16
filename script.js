7fe473ca4174b08d5f04304208188f8e2e947d5b
lyskz <lyski.zach@icloud.com>
2023-02-16 02:50:56 -0800

    wokring on javascript, added some basic variables

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-const start = document.getElementById("start-btn");
+
 const quizQuestionsBox = document.getElementById("question-container");
 const quizQuestion = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
-const leaderBoard = document.getElementById("high-score-board")
+const correct = document.getElementById("correct");
+const incorrect = document.getElementById("incorrect");
 
-var highScoreSubmission = document.getElementById("high-score-submission");
-var scoreBoard
+var hsBoard = document.getElementById("score-board");
+var start = document.getElementById("start-btn");
+var timer = document.getElementById("time-left");
+var countDown;
+var hsSubmission = document.getElementById("high-score-submission");
+var initials = document.getElementById("initials");
+var counter = 90;
+
+
+// changing visibility ***
+//document.getElementById(id).style.visibility = "hidden";
+//document.getElementById(id).style.visibility = "visible";
+
+// eventlistener function to toggle display of begin quiz button...
+
+start.addEventListener("click", startQuiz);
+document.getElementById("high-score-board").addEventListener("click", showScoreBoard);
+
+function showScoreBoard() {
+    hsBoard.classList.remove("hide");
+};
+
+function startQuiz() {
+    start.classList.add("hide");
+};
+
+var questionsArray = [
+
+]
