d7b5cea516983f54d9f98ab1ae0136fff50144e0
lyskz <lyski.zach@icloud.com>
2023-02-17 03:25:00 -0800

    further updated js

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
-
 const quizQuestionsBox = document.getElementById("question-container");
-const quizQuestion = document.getElementById("question");
-const answerButtons = document.getElementById("answer-buttons");
-const correct = document.getElementById("correct");
-const incorrect = document.getElementById("incorrect");
+const answerBtns = document.getElementById("answer-buttons");
+const viewHSBoard = document.getElementById("high-score-board");
 
+var currentQuestionIndex;
+var quizQuestion = document.getElementById("question");
 var hsBoard = document.getElementById("score-board");
 var start = document.getElementById("start-btn");
 var timerEl = document.getElementById("time-left");
-var countDown;
 var hsSubmission = document.getElementById("high-score-submission");
 var initials = document.getElementById("initials");
+var button = document.createElement("button");
+var a1 = document.getElementById("a1");
+var a2 = document.getElementById("a2");
+var selectAnswer;
+var randomQ;
+var countDown;
+var timeRemaining = 90;
 
+var questions = [
+    {   question: "What is 2+2",
+        selectAns: [
+            {text: "2", correct: false},
+            {text: "4", correct: true},
+           
+        ]
+    },
+    {   question: "How old am I?",
+        selectAns: [
+            {text: "25", correct: true},
+            {text: "32", correct: false},
+          
+        ]
 
-
-// changing visibility ***
-//document.getElementById(id).style.visibility = "hidden";
-//document.getElementById(id).style.visibility = "visible";
-
-// eventlistener function to toggle display of begin quiz button...
+    }
+];
 
 start.addEventListener("click", startQuiz);
 document.getElementById("high-score-board").addEventListener("click", showScoreBoard);
@@ -27,17 +42,9 @@ function showScoreBoard() {
     if(hsBoard.classList.value === "hide") {
         hsBoard.classList.remove("hide")}
     else if(hsBoard.classList.value != "hide") {
-        hsBoard.classList.add("hide")
+        hsBoard.classList.add("hide");
     }
-};
-
-function startQuiz() {
-    start.classList.add("hide");
-    timerEl.classList.remove("hide");
-    countDown();
-    quizQuestionsBox.classList.remove("hide");
-};
-
+}
 
 function countDown() {
     var timeRemaining = 90;
@@ -49,4 +56,60 @@ function countDown() {
             clearInterval(timeInterval);
         }
     }, 1000);
-};
+}
+
+function startQuiz() {
+    countDown();
+    start.classList.add("hide");
+    timerEl.classList.remove("hide");
+    quizQuestionsBox.classList.remove("hide");
+    randomQ = questions.sort(() => Math.random() > .5 ? 1 : -1);
+    currentQuestionIndex = 0;
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+    if (currentQuestionIndex >= createdQuestions.length) {
+        clearInterval(countDown);
+    } else {
+        showQuestion(randomizedQ[currentQuestionIndex]);
+    }
+}
+
+function showQuestion(question) {
+    quizQuestion.innerText = question.question
+    
+    let options = questions[currentQuestionIndex].options;
+    options.sort(() => Math.random() > .5 ? 1 : -1);
+
+    a1.textContent = options[0].text;
+    a2.textContent = options[1].text;
+
+    a1.value = options[0].correct;
+    a2.value = options[1].correct;
+}
+
+function selectAnswer(event) {
+    if(event.target.value == "true") {
+        currentQuestionIndex++;
+        setNextQuestion();
+    } else {
+        timeRemaining = timeRemaining -15;
+        currentQuestionIndex++;
+        setNextQuestion();
+    }
+}
+
+function clearData() {
+    while(answerBtns.firstChild) {
+        answerBtns.removeChild(answerBtns.firstChild)
+    }
+}
+
+function submitHS() {
+    quizQuestions.classList.add("hide");
+    hsSubmission.classList.remove("hide");
+    timerEl.textContent =  timeRemaining;
+    submitButton.addEventListener("click");
+}
+
