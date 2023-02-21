6a3197373eb26108e02479b0f4ffe00a7656000c
lyskz <lyski.zach@icloud.com>
2023-02-20 20:37:40 -0800

    near final

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const quizQuestionsBox = document.getElementById("question-container");
 const answerBtns = document.getElementById("answer-buttons");
-const viewHSBoard = document.getElementById("high-score-board");
-const isTrue = true;
 
 var currentQuestionIndex;
 var quizQuestion = document.getElementById("question");
@@ -10,7 +8,7 @@ var start = document.getElementById("start-btn");
 var timerEl = document.getElementById("time-left");
 var submitBtn = document.getElementById("submit-button");
 var hsSubmission = document.getElementById("high-score-submission");
-var initials = document.getElementById("initials");
+var initials = document.querySelector("#initials");
 var button = document.createElement("button");
 var a1 = document.getElementById("a1");
 var a2 = document.getElementById("a2");
@@ -21,21 +19,23 @@ var timeRemaining = 90;
 var currentQuestionIndex = 0;
 var finalScore = document.getElementById("time-remaining");
 
-const highScoresList = document.getElementById("hs-board-list");
+
+const endHighScores = JSON.parse(localStorage.getItem("endHighScores")) || [];
+var highScoresList = document.getElementById("hs-board-list");
 
 
 var questions = [
     {   question: "What is 2+2",
         options: [
-            {text: "2", correct: false},
-            {text: "4", correct: true},
+            {text: "2"},
+            {text: "4"},
            
         ]
     },
     {   question: "How old am I?",
         options: [
-            {text: "25", correct: true},
-            {text: "32", correct: false},
+            {text: "25"},
+            {text: "32", },
           
         ]
 
@@ -100,14 +100,22 @@ function initEnd() {
     hsSubmission.classList.remove("hide");
     timerEl.classList.add("hide");
     finalScore.textContent = "Your score is: " + timeRemaining;
-    submitBtn.addEventListener("click", HSBoard);
+
+    var user = {
+        name: initials.value.trim(),
+        score: timeRemaining
+    };
+    localStorage.setItem("user", JSON.stringify(user));
+
+    submitBtn.addEventListener("click", showHSBoard);
 }
 
-function HSBoard() {
+function showHSBoard() {
     hsSubmission.classList.add("hide");
     hsBoard.classList.remove("hide");
-}
+    highScoresList.textContent = localStorage.getItem("user");
 
+}
 
 function timer() {
     timeRemaining--;
